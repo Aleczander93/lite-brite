f465e3060fb4b0cd0fd593d46643a59e9381fb6a
aleczander <afigueroa81@ufl.edu>
2016-10-05 19:48:40 -0400

    created grid with js, css and some html

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,17 @@
+(function() {
+  var numberOfRows = 40; //number of rows in the grid
+  var numberOfCols = 40; //number of columns in the grid
+  var canvas = $('#canvas'); //my placement are + think of paper in drawing
 
-alert ('no stars no bullshit');
+  //let's make some rows and put them in the body
+for(var rowCount = 0; rowCount < numberOfRows; rowCount += 1){
+  var row = $('<tr></tr>');
+  for(var colCount= 0; colCount < numberOfCols; colCount+=1){
+  var column = $('<td></td>');
+  column.addClass('cell');
+  row.append(column);
+  }
+  canvas.append(row);
+}
+
+}());

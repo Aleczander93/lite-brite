d706a9c96bd026297f10db4c617f0efa87b5910b
aleczander <afigueroa81@ufl.edu>
2016-10-05 20:34:14 -0400

    base functionality for changing cell color

--- public/js/app.js
+++ public/js/app.js
@@ -3,15 +3,28 @@
   var numberOfCols = 40; //number of columns in the grid
   var canvas = $('#canvas'); //my placement are + think of paper in drawing
 
+
+
+makeGrid();
+var cells = $('.cell');
+$('.cell').on('mouseover', changeColor);
+
+function changeColor(event){
+  //just this cell's background
+  $(this).toggleClass('red');
+}
+
+function makeGrid(){
   //let's make some rows and put them in the body
 for(var rowCount = 0; rowCount < numberOfRows; rowCount += 1){
   var row = $('<tr></tr>');
   for(var colCount= 0; colCount < numberOfCols; colCount+=1){
-  var column = $('<td></td>');
-  column.addClass('cell');
-  row.append(column);
+    var column = $('<td></td>');
+    column.addClass('cell');
+    row.append(column);
   }
-  canvas.append(row);
-}
+    canvas.append(row);
+    }
 
-}());
+}
+  }());

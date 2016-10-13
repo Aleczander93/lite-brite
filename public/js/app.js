d54b7e3735b04c83dccffcca369fe40ddca640c9
aleczander <afigueroa81@ufl.edu>
2016-10-13 18:33:43 -0400

    created buttons for colors and attempted creating a clear button

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,23 @@
 (function() {
+
+
   var canvas = $('#canvas'); //my placement are + think of paper in drawing
   var updateGridButton = $ ('#update-grid-button');
   var numberOfColsInput = $('#number-of-cols');
   var numberOfRowsInput = $('#number-of-rows');
   var blueButton = $('#blue-button');
+  var greenButton = $('#green-button');
+  var orangeButton = $('#orange-button');
 
-makeGrid(15, 15);
-
+  makeGrid(15, 15);
 $('.cell').on('click', changeColor);
 blueButton.on('click', blueButtonOn);
+greenButton.on('click', greenButtonOn);
+orangeButton.on('click', orangeButtonOn);
 updateGridButton.on('click', updateGridSize);
 // updateGridButton.attr("disabled", false);
 // clearGrid();
 // makeGrid(30, 30);
-
 //if column || rows = nothing --> do not update
 
 function updateGridSize() {
@@ -26,6 +30,8 @@ function updateGridSize() {
   makeGrid(newRowNumber, newColumnNumber);
   $('.cell').on('click', changeColor);
   $('.cell').on('click', blueButtonOn);
+  $('.cell').on('click', greenButtonOn);
+  $('.cell').on('click', orangeButtonOn);
 }
 
 function clearGrid(){
@@ -38,15 +44,29 @@ function changeColor(event){
 }
 
 function blueButtonOn(event){
+  $('.cell').off('click');
   $('.cell').on('click', function(){
   $(this).toggleClass('blue');
 });
+}
 
+function greenButtonOn(event){
+  $('.cell').off('click');
+  $('.cell').on('click', function(){
+  $(this).toggleClass('green');
+});
+}
+
+function orangeButtonOn(event){
+  $('.cell').off('click');
+  $('.cell').on('click', function(){
+    $(this).toggleClass('orange');
+});
 }
 
 function makeGrid(numberOfRows, numberOfCols){
   //let's make some rows and put them in the body
-for(var rowCount = 0; rowCount < numberOfRows; rowCount += 1){
+for(var rowCount = 0; rowCount < numberOfRows; rowCount+=1){
   var row = $('<tr></tr>');
   for(var colCount= 0; colCount < numberOfCols; colCount+=1){
     var column = $('<td></td>');
